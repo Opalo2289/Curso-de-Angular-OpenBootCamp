d590c5464eafe34d3a0f0cbb0b75a768f43ee126
Opalo1189 <enriquemaestre11@gmail.com>
2023-01-23 17:46:09 -0500

    Sesion 1 HolaMundo

--- HolaMundo/src/app/app.component.ts
+++ HolaMundo/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'app-root', //Esto el nombre que tiene que renderizar o mostrar en el index.html para que sea mostrado en vista
+  templateUrl: './app.component.html', //Esto es la plantilla 
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'HolaMundo';
+  nombre = 'Guedys'
 }
