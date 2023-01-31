6de287e4cf6bb623ffbf840ab2d6a8ef3c8a6ac0
Opalo1189 <enriquemaestre11@gmail.com>
2023-01-31 16:41:36 -0500

    Finalizado modulo de Directivas

--- HolaMundo/src/app/app.module.ts
+++ HolaMundo/src/app/app.module.ts
@@ -4,15 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SaludoComponent } from './components/saludo/saludo.component';
+import { ListaBasicaComponent } from './modules/lists/lista-basica/lista-basica.component';
+
+//Modulo personalizado que exporta componentes de tipo lista
+import { ListsModule } from './modules/lists/lists.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SaludoComponent
+    SaludoComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    /*Aqui importamos los modulos que se crean personalizados
+    en este caso importamos List.module.ts.
+    los modulos que se importan aqui, luego se pueden
+    utilizar en cualquier modulo o componente que se encuentre en <<declarations>>
+    */
+   ListsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
